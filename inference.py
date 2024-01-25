a4ebb1afa40c4ce14c157de54f94906d50c3b7c6
Kangnam Kim <kangnam7654@naver.com>
2024-01-25 10:40:31 +0900

    added prediction

--- inference.py
+++ inference.py
@@ -1,4 +1,5 @@
 import argparse
+from tqdm import tqdm 
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
@@ -10,7 +11,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--weight", type=str)
     parser.add_argument("--csv", type=str)
-    parser.add_argument("--batch_size", type=int, default=1024)
+    parser.add_argument("--batch_size", type=int, default=1)
     args = parser.parse_args()
     return args
 
@@ -26,10 +27,21 @@ def main(args):
     dataset = DataModule(df, train=False)
     loader = DataLoader(dataset, batch_size=args.batch_size)
 
-    for data in loader:
-        out = model(data)
+    predictions = []
+    for data in tqdm(loader):
+        logit = model(data)
+        if logit.item() >= 0.5:
+            out = 1
+        else:
+            out = 0
+        predictions.append(out)
+    df["class"] = predictions
+    df.to_csv("./data/prediction.csv")
+    pass
 
 
 if __name__ == "__main__":
     args = get_args()
+    args.weight = "/home/kangnam/project/Novelis/weights/regressor.pt"
+    args.csv = "/home/kangnam/project/Novelis/data/test.csv"
     main(args)
